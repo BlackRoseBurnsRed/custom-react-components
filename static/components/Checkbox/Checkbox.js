7f3039bce40d82312adf307f29e7e7d0f7e8b5a8
z.ovcharov <z.ovcharov@corp.youdo.com>
2018-03-19 19:38:43 +0300

    Make checkbox files

---
+++ static/components/Checkbox/Checkbox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './checkbox.css'
+
+export class Checkbox extends React.Component {
+    render() {
+        return (
+            <label>
+                <input class="checkbox" type="checkbox" name="checkbox-test" />
+                    <span class="checkbox-custom"></span>
+                    <span class="label">{this.props.label}</span>
+            </label>
+        )
+    }
+}
--- static/kit.js
+++ static/kit.js
@@ -1,10 +1,15 @@
 import {Button} from "./components/Button/Button";
+import {Checkbox} from "./components/Checkbox/Checkbox";
 import React from 'react';
 
 export class Kit extends React.Component {
     render() {
         return (
-            <Button onClick={() => {alert('Clicked')}}/>
+            <div>
+                <Checkbox label='custom checkbox'/>
+                <br/>
+                <Button onClick={() => {alert('Clicked')}}/>
+            </div>
         )
     }
 }
